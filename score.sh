4d3bc168c64a887fad73f2d99b16d4492e0fd9ca
yunhai <haihai107@126.com>
2020-06-27 22:54:35 -0700

    add time-scheduled scripts

---
+++ score.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+day=$(date +"%Y-%m-%d")
+data=$(curl -s 'https://www.cnblogs.com/goodcitizen/ajax/sidecolumn.aspx' | grep 'liScore' -A 6 | sed -n -e 3p -e 7p)
+score=$(echo $data | sed -n 1p)
+rank=$(echo $data | sed -n 2p)
+echo "$day $score $rank" >> score.txt
+
