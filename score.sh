7e362cf142e2d1303e24332cea144b72594c080b
yunh <yunh@glodon.com>
2020-06-29 10:07:22 +0800

    check if updated already

--- score.sh
+++ score.sh
@@ -1,5 +1,12 @@
 #! /bin/sh
+git pull origin master
 day=$(date +"%Y-%m-%d")
+last=$(cat ./score.txt | tail -1 | awk '{ print $1 }')
+if [[ "$day" < "$last" ]] || [[ "$day" = "$last" ]]; then
+    echo "has updated, skip"
+    exit 0
+fi
+
 data=$(curl -s 'https://www.cnblogs.com/goodcitizen/ajax/sidecolumn.aspx' | grep 'liScore' -A 6 | sed -n -e 3p -e 7p)
 score=$(echo $data | sed -n 1p)
 rank=$(echo $data | sed -n 2p)
