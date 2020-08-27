72bfa3279d1dd522e4c00c0fafe66f57cb529608
yunh <yunh@glodon.com>
2020-08-27 15:07:17 +0800

    open draw.png on vary platform

--- plot.sh
+++ plot.sh
@@ -12,4 +12,4 @@ echo "y1 range [$y1min,$y1max], y2 range [$y2max,$y2min]"
 #y2max=$(($y2max+500))
 gnuplot -e "usr='$usr'" -e "y1min='$y1min'" -e "y1max='$y1max'" -e "y2max='$y2max' " -e "y2min='$y2min'" ./draw.plt
 eog draw.png &
-#mspaint draw.png &
+mspaint draw.png &
