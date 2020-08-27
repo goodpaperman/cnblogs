1374a64092e1a429756c8f71aaead0b19e289ec2
yunh <yunh@glodon.com>
2020-08-27 15:06:47 +0800

    dump y1 & y2 ranges

--- plot.sh
+++ plot.sh
@@ -7,9 +7,9 @@ y2max=$(echo $firstline | awk '{ print $3 }')
 lastline=$(cat ./score.txt | tail -1)
 y1max=$(echo $lastline | awk '{ print $2 }')
 y2min=$(echo $lastline | awk '{ print $3 }')
-echo "y1 min $y1min, y2 max $y2max"
+echo "y1 range [$y1min,$y1max], y2 range [$y2max,$y2min]"
 #y1min=$(($y1min-50))
 #y2max=$(($y2max+500))
-gnuplot -e "usr='$usr'" -e "y1min='$y1min'" -e "y1max='$y1max'" -e "y2min='$y2min'" -e "y2max='$y2max'" ./draw.plt
+gnuplot -e "usr='$usr'" -e "y1min='$y1min'" -e "y1max='$y1max'" -e "y2max='$y2max' " -e "y2min='$y2min'" ./draw.plt
 eog draw.png &
 #mspaint draw.png &
