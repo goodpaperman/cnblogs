e2e1e54de65663bf8c0934936f93581e10f70b50
yunh <yunh@glodon.com>
2021-01-19 10:17:12 +0800

    add missing file

---
+++ gpush.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/expect -f
+set timeout 30
+for {set i 0} {<=10} {incr i} {
+    puts "start pushing git "
+    spawn git push origin master
+    expect "Everything up-to-date" { puts "pushing ok"; exit }
+}
