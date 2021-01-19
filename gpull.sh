056eb04ba2162b155084240a1253519dfcdcfbbc
yunh <yunh@glodon.com>
2021-01-19 10:18:38 +0800

    can run on kylin loongson

--- gpull.sh
+++ gpull.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/expect -f
 set timeout 30
-for {set i 0} {<=10} {incr i} {
+for {set i 0} {$i<=10} {incr i} {
     puts "start pulling git "
     spawn git pull origin master
     expect "Already up-to-date." { puts "pulling ok"; exit }
--- gpush.sh
+++ gpush.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/expect -f
 set timeout 30
-for {set i 0} {<=10} {incr i} {
+for {set i 0} {$i<=10} {incr i} {
     puts "start pushing git "
     spawn git push origin master
     expect "Everything up-to-date" { puts "pushing ok"; exit }
